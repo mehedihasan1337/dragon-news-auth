0301ff55dcd1141d022ba034f93ff8617475ef89
mehedihasan1337 <mdmehedihasan1337@gmail.com>
2024-11-15 00:34:55 +0600

    added a all file

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
     return (
         <div>
-            Auth layout
+            Auth Layout 
             <Outlet></Outlet>
         </div>
     );
